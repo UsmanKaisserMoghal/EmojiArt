f43edbf042aa18d57312fe9e741e46d30cc75c88
MBP <mbp@MBPs-MacBook-Pro.local>
2019-01-10 13:08:01 +0500

    populate collection view

--- EmojiArt/EmojiArtViewController.swift
+++ EmojiArt/EmojiArtViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScrollViewDelegate {
+class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScrollViewDelegate, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     
     @IBOutlet weak var spinner: UIActivityIndicatorView!
@@ -30,12 +30,6 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScr
         }
     }
     
-    
-    @IBOutlet weak var scrollViewWidth: NSLayoutConstraint!
-    
-    @IBOutlet weak var scrollViewHeight: NSLayoutConstraint!
-    
-    
     var emojiArtView = EmojiArtView()
     
     @IBOutlet weak var scrollView: UIScrollView! {
@@ -45,11 +39,6 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScr
             scrollView.delegate = self
             scrollView.addSubview(emojiArtView)
         }
-    }
-    
-    func scrollViewDidZoom(_ scrollView: UIScrollView) {
-        scrollViewHeight.constant = scrollView.contentSize.height
-        scrollViewWidth.constant = scrollView.contentSize.width
     }
     
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
@@ -66,8 +55,6 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScr
             let size = newValue?.size ?? CGSize.zero
             emojiArtView.frame = CGRect(origin: CGPoint.zero, size: size)
             scrollView?.contentSize = size
-            scrollViewHeight?.constant = size.height
-            scrollViewWidth?.constant = size.width
             if let dropZone = self.dropZone, size.width > 0, size.height > 0 {
                 scrollView?.zoomScale = max(dropZone.bounds.size.width / size.width, dropZone.bounds.size.height / size.height)
             }
@@ -80,6 +67,35 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScr
             dropZone.addInteraction(UIDropInteraction(delegate: self))
         }
     }
+    
+    var emojis = "🏐🏉🎱🐹🐮🐼🐵😍🤓".map {String($0)}
+    
+    
+    @IBOutlet weak var emojiCollectionView: UICollectionView! {
+        didSet {
+            emojiCollectionView.dataSource = self
+            emojiCollectionView.delegate = self
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    private var font: UIFont {
+        return UIFontMetrics(forTextStyle: .body).scaledFont(for: UIFont.preferredFont(forTextStyle: .body).withSize(64.0))
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "EmojiCell", for: indexPath)
+        if let emojiCell = cell as? EmojiCollectionViewCell {
+            let text = NSAttributedString(string: emojis[indexPath.item], attributes: [.font:font])
+            emojiCell.label.attributedText = text
+        }
+        return cell
+    }
+    
+    
     
     func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
         return session.canLoadObjects(ofClass: NSURL.self) && session.canLoadObjects(ofClass: UIImage.self)
---
+++ EmojiArt/EmojiCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by MBP on 10/01/2019.
+//  Copyright © 2019 MBP. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+}
